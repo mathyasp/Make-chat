35dde78a624f0a28dbae01d212883979c85c4280
mathyasp <mathyastp@gmail.com>
2024-06-21 10:44:48 -0400

    Integrated Socket.io

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-//App.js
+//app.js
 const express = require('express');
 const app = express();
-//Socket.io has to use the http server
 const server = require('http').Server(app);
 
-//Express View Engine for Handlebars
-const exphbs  = require('express-handlebars');
-app.engine('handlebars', exphbs());
+//Socket.io
+const io = require('socket.io')(server);
+io.on("connection", (socket) => {
+  console.log("🔌 New user connected! 🔌");
+});
+
+const { engine } = require('express-handlebars');
+app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
+app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('index.handlebars');
-})
+});
 
 server.listen('3000', () => {
   console.log('Server listening on Port 3000');
-})
+});
