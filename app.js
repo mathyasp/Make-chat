8867a16b22a1df67eb276c903fcfab60b79ed038
mathyasp <mathyastp@gmail.com>
2024-06-21 16:46:53 -0400

    Users can create new channels and join

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 //We'll store our online users here
 let onlineUsers = {};
+let channels = {"General" : []};
 io.on("connection", (socket) => {
   // Make sure to send the users to our chat file
-  require('./sockets/chat.js')(io, socket, onlineUsers);
-})
+  require('./sockets/chat.js')(io, socket, onlineUsers, channels);
+});
 
 const { engine } = require('express-handlebars');
 app.engine('handlebars', engine());
